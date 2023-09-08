7130c87b336c47c12f15786bbe93b18bef72d96d
scapula07 <bartholomewonogwu@yahoo.com>
2023-09-08 22:46:27 +0100

    dashboard logo1

--- src/modules/Dashboard/components/friends/index.jsx
+++ src/modules/Dashboard/components/friends/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import post from "../../../../assets/post.png"
 
 export default function Friends() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col w-full'>
+        <div className='w-full'>
+            <h5 className='text'>Friends Request</h5>
+            <h5>See all</h5>
+
+        </div>
         {[].map(()=>{
              return(
-                <div>
+                <div className='flex items-center'>
+
+
                 </div>
              )
         })}
